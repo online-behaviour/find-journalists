9c6cfbfa3a29006eccf739b78b9c485cbfedbe8b
eriktks <erikt@xs4all.nl>
2018-03-09 20:22:07 +0100

    checked for duplicate ids

--- tweets2hours.py
+++ tweets2hours.py
@@ -11,50 +11,56 @@ import re
 import sys
 
 # constants
-COMMAND = sys.argv
+COMMAND = sys.argv[0]
 # hours to subtract/add to get real time from GMT in tweets
-DELTAHOURS = 2
+DELTAHOURS = 1
 # list with open file handles
 files = {}
+# tweet id list
+seen = {}
 
 # read a tweet
 for line in sys.stdin:
     # remove final newline character
     line = line.rstrip()
     # convert line to json
-    try: 
-        jsonLine = json.loads(line)
-    except:
-        sys.exit(COMMAND+": unknown format of input line: "+line+"\n")
+    try: jsonLine = json.loads(line)
+    except: continue
     if not "created_at" in jsonLine:
         sys.stderr.write(COMMAND+": skipping line (no date found): "+line+"\n")
         continue
-    # parse the date string
-    date = time.strptime(jsonLine["created_at"].encode("utf-8"),"%a %b %d %H:%M:%S +0000 %Y")
-    # make the file name
-    # get real local time from GMT in tweets
-    month = date[1]
-    day = date[2]
-    hour = date[3]+DELTAHOURS
-    while hour >= 24:
-        hour -= 24
-        day += 1 # this may result in an invalid date
-    while hour < 0:
-        hour += 24
-        day -= 1 # this may result in an invalid date
-    if month > 0 and month < 10: month = "0"+str(month)
-    else: month = str(month)
-    if day > 0 and day < 10: day = "0"+str(day)
-    else: day = str(day)
-    if hour < 10: hour = "0"+str(hour)
-    else: hour = str(hour)
-    thisFile = str(date[0])+month+day+"-"+hour+".out"
-    # open file
-    if not thisFile in files:
-        outFile = open(thisFile,"w")
-        files[thisFile] = outFile
-    # write the tweet to the file
-    files[thisFile].write(line+"\n")
+    if not "id_str" in jsonLine:
+        sys.stderr.write(COMMAND+": skipping line (no id_str found): "+line+"\n")
+        continue
+    id = jsonLine["id_str"]
+    if not id in seen:
+        seen[id] = True
+        # parse the date string
+        date = time.strptime(jsonLine["created_at"].encode("utf-8"),"%a %b %d %H:%M:%S +0000 %Y")
+        # make the file name
+        # get real local time from GMT in tweets
+        month = date[1]
+        day = date[2]
+        hour = date[3]+DELTAHOURS
+        while hour >= 24:
+            hour -= 24
+            day += 1 # this may result in an invalid date
+        while hour < 0:
+            hour += 24
+            day -= 1 # this may result in an invalid date
+        if month > 0 and month < 10: month = "0"+str(month)
+        else: month = str(month)
+        if day > 0 and day < 10: day = "0"+str(day)
+        else: day = str(day)
+        if hour < 10: hour = "0"+str(hour)
+        else: hour = str(hour)
+        thisFile = str(date[0])+month+day+"-"+hour+".out"
+        # open file
+        if not thisFile in files:
+            outFile = open(thisFile,"w")
+            files[thisFile] = outFile
+        # write the tweet to the file
+        files[thisFile].write(line+"\n")
 
 # done: close all file handles
 for fileName in files: files[fileName].close()
